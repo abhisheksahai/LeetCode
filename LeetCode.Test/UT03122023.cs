6e2e8f5210097c4d71b2ccc962b49347c3579e35
Abhishek Sahai <abhishek.sahai@outlook.in>
2023-12-03 20:30:09 +0530

    ElementsValueEqualToIndex

---
+++ LeetCode.Test/UT03122023.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Test
+{
+    public class UT03122023
+    {
+        [Test]
+        public void ElementsValueEqualToIndexTest()
+        {
+            var result = LC03122023.ElementsValueEqualToIndex([15, 2, 45, 12, 7]);
+        }
+    }
+}
---
+++ LeetCode/LC03122023.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode
+{
+    public static class LC03122023
+    {
+        public static int[] ElementsValueEqualToIndex(int[] nums)
+        {
+            List<int> result = [];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == i + 1)
+                {
+                    result.Add(nums[i]);
+                }
+            }
+            return result.ToArray();
+        }
+    }
+}
