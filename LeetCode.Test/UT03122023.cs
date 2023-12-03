39fc9bef8839673fe1831b89c437a36f03294ee3
Abhishek Sahai <abhishek.sahai@outlook.in>
2023-12-03 22:24:27 +0530

    RemoveAllMaximumMinimumTest

--- LeetCode.Test/UT03122023.cs
+++ LeetCode.Test/UT03122023.cs
@@ -7,5 +7,17 @@
         {
             var result = LC03122023.ElementsValueEqualToIndex([15, 2, 45, 12, 7]);
         }
+
+        [Test]
+        public void PerFectArrayTest()
+        {
+            LC03122023.PerFectArray([1, 2, 3, 2, 1]);
+        }
+
+        [Test]
+        public void RemoveAllMaximumMinimumTest()
+        {
+            int result = LC03122023.RemoveAllMaximumMinimum([7, 8, 3, 4, 2, 9, 5]);
+        }
     }
 }
--- LeetCode/LC03122023.cs
+++ LeetCode/LC03122023.cs
@@ -14,5 +14,48 @@
             }
             return result.ToArray();
         }
+
+        public static void PerFectArray(int[] nums)
+        {
+            int len = nums.Length;
+            for (int i = 0; i < len / 2; i++)
+            {
+                if (nums[i] != nums[len - 1 - i])
+                {
+                    Console.WriteLine("Not perfect");
+                    return;
+                }
+            }
+            Console.WriteLine("Perfect");
+        }
+
+        public static int RemoveAllMaximumMinimum(int[] nums)
+        {
+            List<int> result = [];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result.Add(nums[i]);
+            }
+            while (result.Count > 1)
+            {
+                int min = int.MaxValue;
+                int max = int.MinValue;
+                for (int i = 0; i < result.Count; i++)
+                {
+                    if (result[i] < min)
+                    {
+                        min = result[i];
+                    }
+                    if (result[i] > max)
+                    {
+                        max = result[i];
+                    }
+                }
+                result.Remove(min);
+                result.Remove(max);
+            }
+            Console.WriteLine($"{result[0]}");
+            return result[0];
+        }
     }
 }
