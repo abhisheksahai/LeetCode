3cddb32a4b95154d6d5172fc0f4568e7f800af17
Abhishek Sahai <abhishek.sahai@outlook.in>
2023-12-09 22:26:46 +0530

    Assignment

--- LeetCode.Test/UT09122023.cs
+++ LeetCode.Test/UT09122023.cs
@@ -14,5 +14,47 @@
         {
             var result = LC09122023.FizzBuzz(15);
         }
+
+        [Test]
+        public void MaxOfThreeTest()
+        {
+            var result = LC09122023.MaxOfThree(3, -9, 88);
+        }
+
+        [Test]
+        public void MinOfThreeTest()
+        {
+            var result = LC09122023.MinOfThree(3, -9, 88);
+        }
+
+        [Test]
+        public void MidOfThreeTest()
+        {
+            var result = LC09122023.MidOfThree(3, -9, 88);
+        }
+
+        [Test]
+        public void PrintNumberSeriesTest()
+        {
+            LC09122023.PrintNumberSeries(5);
+        }
+
+        [Test]
+        public void PrintReverseNumberSeries()
+        {
+            LC09122023.PrintReverseNumberSeries(5);
+        }
+
+        [Test]
+        public void ReturnOddElementsTest()
+        {
+            var result = LC09122023.ReturnOddElements([1, 5, 6, 4, 3, 2, 8]);
+        }
+
+        [Test]
+        public void SumOfElementTest()
+        {
+            int result = LC09122023.SumOfElement([1, 2, 6, 3, 5]);
+        }
     }
 }
--- LeetCode/LC09122023.cs
+++ LeetCode/LC09122023.cs
@@ -53,5 +53,73 @@
             }
             return result;
         }
+
+        public static int MaxOfThree(int a, int b, int c)
+        {
+            return Math.Max(Math.Max(a, b), c);
+        }
+
+        public static int MinOfThree(int a, int b, int c)
+        {
+            return Math.Min(Math.Min(a, b), c);
+        }
+
+        public static int MidOfThree(int a, int b, int c)
+        {
+            if (a > b && a < c || a > c && a < b)
+                return a;
+            if (b > c && b < a || b > a && b < c)
+                return b;
+            return c;
+        }
+
+        public static void PrintNumberSeries(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                int counter = 1;
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(counter++);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void PrintReverseNumberSeries(int num)
+        {
+            for (int i = num; i >= 1; i--)
+            {
+                int counter = 1;
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(counter++);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static int[] ReturnOddElements(int[] nums)
+        {
+            IList<int> result = new List<int>();
+            foreach (var num in nums)
+            {
+                if (num % 2 == 1)
+                {
+                    result.Add(num);
+                }
+            }
+            return result.ToArray();
+        }
+
+        public static int SumOfElement(int[] nums)
+        {
+            int result = 0;
+            foreach (var num in nums)
+            {
+                result += num;
+            }
+            return result;
+        }
     }
 }
