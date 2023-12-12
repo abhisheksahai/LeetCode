38d74372ba1c20b669ca20a58c1b51bc1c25d338
Abhishek Sahai <abhishek.sahai@outlook.in>
2023-12-12 22:24:40 +0530

    Intersection

---
+++ LeetCode.Test/UT12122023.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Test
+{
+    public class UT12122023
+    {
+        [Test]
+        public void IsPalindromeTest()
+        {
+            bool result = LC12122023.IsPalindrome(121);
+        }
+
+        [Test]
+        public void IntersectionTest()
+        {
+            var result = LC12122023.Intersection([1, 2, 2, 1], [2, 2]);
+        }
+    }
+}
---
+++ LeetCode/LC12122023.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode
+{
+    public class LC12122023
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/palindrome-number/
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static bool IsPalindrome(int x)
+        {
+            int y = x, z = 0;
+            while (y > 0)
+            {
+                z = z * 10 + y % 10;
+                y = y / 10;
+            }
+            return x == z;
+        }
+
+        public static int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> result = new();
+            HashSet<int> elements = new();
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                if (!elements.Contains(nums1[i]))
+                {
+                    elements.Add(nums1[i]);
+                }
+            }
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                if (elements.Contains(nums2[i]) && !result.Contains(nums2[i]))
+                {
+                    result.Add(nums2[i]);
+                }
+            }
+            return [.. result];
+        }
+    }
+}
