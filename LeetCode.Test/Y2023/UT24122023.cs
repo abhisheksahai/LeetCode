ceaffa80a409269fb913aef15924340e2610800c
Abhishek Sahai <abhishek.sahai@outlook.in>
2023-12-27 23:03:57 +0530

    4 Sum

--- LeetCode.Test/Y2023/UT24122023.cs
+++ LeetCode.Test/Y2023/UT24122023.cs
@@ -15,7 +15,15 @@ namespace LeetCode.Test.Y2023
         public void ThreeSumClosestTest()
         {
             var result1 = LC24122023.ThreeSumClosest([-1, 2, 1, -4], 1);
-            var result2 = LC24122023.ThreeSumClosest([1,1,1,1], 4);
+            var result2 = LC24122023.ThreeSumClosest([1, 1, 1, 1], 4);
+        }
+
+        [Test]
+        public void FourSumTest()
+        {
+            var result1 = LC24122023.FourSum([1000000000, 1000000000, 1000000000, 1000000000], -294967296);
+            var result2 = LC24122023.FourSum([2, 2, 2, 2, 2], 8);
+            var result3 = LC24122023.FourSum([1, 0, -1, 0, -2, 2], 0);
         }
     }
 }
--- LeetCode/Y2023/LC24122023.cs
+++ LeetCode/Y2023/LC24122023.cs
@@ -109,5 +109,65 @@
             }
             return target + diff;
         }
+
+        /// <summary>
+        /// https://leetcode.com/problems/4sum
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            if (nums == null || nums.Length < 4)
+            {
+                return result;
+            }
+            int len = nums.Length;
+            Array.Sort(nums);
+            for (int i = 0; i < len - 3; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+                for (int j = i + 1; j < len - 2; j++)
+                {
+                    if (j > i + 1 && nums[j] == nums[j - 1])
+                    {
+                        continue;
+                    }
+                    int left = j + 1, right = len - 1;
+                    long sum = nums[i] + nums[j];
+                    while (left < right)
+                    {
+                        long totalSum = sum + nums[left] + nums[right];
+                        if (totalSum < target)
+                        {
+                            left++;
+                        }
+                        else if (totalSum > target)
+                        {
+                            right--;
+                        }
+                        else
+                        {
+                            result.Add(new List<int>() { nums[i], nums[j], nums[left], nums[right] });
+                            left++;
+                            right--;
+                            while (left < right && nums[left] == nums[left - 1])
+                            {
+                                left++;
+                            }
+                            while (left < right && nums[right] == nums[right + 1])
+                            {
+                                right--;
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
     }
 }
