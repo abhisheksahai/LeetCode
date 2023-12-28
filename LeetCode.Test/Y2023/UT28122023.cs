c1e39b872ee65366cdf9e87e35420948dd1a18f9
Abhishek Sahai <abhishek.sahai@outlook.in>
2023-12-29 00:16:00 +0530

    Array assignment 2

---
+++ LeetCode.Test/Y2023/UT28122023.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Y2023;
+
+namespace LeetCode.Test.Y2023
+{
+    public class UT28122023
+    {
+        [Test]
+        public void MoveZeroesTest()
+        {
+            LC28122023.MoveZeroes([0, 1, 0, 3, 12]);
+        }
+
+        [Test]
+        public void ContainsDuplicateTest()
+        {
+            var result = LC28122023.ContainsDuplicate([1, 2, 3, 1]);
+        }
+
+        [Test]
+        public void ContainsNearbyDuplicateTest()
+        {
+            var result = LC28122023.ContainsNearbyDuplicate([1, 2, 3, 1], 3);
+        }
+    }
+}
---
+++ LeetCode/Y2023/LC28122023.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Y2023
+{
+    public class LC28122023
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/move-zeroes
+        /// TC=O(N), SC=O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        public static void MoveZeroes(int[] nums)
+        {
+            if (nums is null || nums.Length == 0) return;
+            int k = 0, len = nums.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[k++] = nums[i];
+                }
+            }
+            while (k < len)
+            {
+                nums[k++] = 0;
+            }
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/contains-duplicate/
+        /// TC=O(N) and SC=O(N)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> set = new();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (set.Contains(nums[i]))
+                {
+                    return true;
+                }
+                set.Add(nums[i]);
+            }
+            return false;
+        }
+
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            int len = nums.Length;
+
+            return false;
+        }
+    }
+}
