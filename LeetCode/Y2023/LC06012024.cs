191aa3036211c57b4158e7a41ba3782630ee8e05
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-01-08 23:07:58 +0530

    Trap rain water

--- LeetCode/Y2023/LC06012024.cs
+++ LeetCode/Y2023/LC06012024.cs
@@ -84,40 +84,58 @@
 
         /// <summary>
         /// https://leetcode.com/problems/merge-intervals/
+        /// TC=O(N) and SC=O(N)
         /// Salesforce,MS,Amazon
         /// </summary>
         /// <param name="intervals"></param>
         /// <returns></returns>
         public static int[][] Merge(int[][] intervals)
         {
-            List<int[]> result = new();
             intervals = intervals.OrderBy(x => x[0]).ToArray();
+            List<int[]> result = [];
             result.Add(intervals[0]);
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                int lastCount = result.Count - 1;
+                if (result[lastCount][1] >= intervals[i][0])
+                {
+                    result[lastCount][1] = Math.Max(intervals[i][1], result[lastCount][1]);
+                }
+                else
+                {
+                    result.Add(intervals[i]);
+                }
+            }
             return result.ToArray();
         }
 
         /// <summary>
         /// https://leetcode.com/problems/product-of-array-except-self
         /// Approach : for index i the value will product of its left element * product of its right element
+        /// TC=O(N) and SC=O(N)
         /// </summary>
         /// <param name="nums"></param>
         /// <returns></returns>
         public static int[] ProductExceptSelf(int[] nums)
         {
             int len = nums.Length;
-            int[] res = new int[len];
-            res[0] = 1;
+            int[] left = new int[len];
+            int[] right = new int[len];
+            int temp = 1;
+            left[0] = 1;
             for (int i = 1; i < len; i++)
             {
-                res[i] = res[i - 1] * nums[i - 1];
+                temp = temp * nums[i - 1];
+                left[i] = temp;
             }
-            int right = 1;
-            for (int i = len - 1; i >= 0; i--)
+            temp = 1;
+            right[len - 1] = 1;
+            for (int i = len - 2; i > 0; i--)
             {
-                res[i] *= right;
-                right *= nums[i];
+                temp = temp * nums[i + 1];
             }
-            return res;
+            return left;
+
         }
 
         /// <summary>
@@ -128,8 +146,29 @@
         public static int Trap(int[] height)
         {
             int result = 0;
+            int len = height.Length;
+            int[] leftMax = new int[len];
+            int[] rightMax = new int[len];
+            leftMax[0] = height[0];
+            
 
+            for (int i = 1; i < len; i++)
+            {
+                leftMax[i] = Math.Max(leftMax[i - 1], height[i]);
+            }
+
+            rightMax[len - 1] = height[len - 1];
+            for (int i = len - 2; i >= 0; i--)
+            {
+                rightMax[i] = Math.Max(rightMax[i + 1], height[i]);
+            }
+
+            for (int i = 0; i < len; i++)
+            {
+                result += (Math.Min(leftMax[i], rightMax[i]) - height[i]);
+            }
             return result;
         }
+
     }
 }
