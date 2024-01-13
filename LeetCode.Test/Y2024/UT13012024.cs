0dc77ddc4719ec9a878ba5db533eeb751bc375b8
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-01-13 09:23:54 +0530

    LongestKSubstrTest

--- LeetCode.Test/Y2024/UT13012024.cs
+++ LeetCode.Test/Y2024/UT13012024.cs
@@ -1,7 +1,13 @@
-﻿namespace LeetCode.Test.Y2024
+﻿using LeetCode.Y2024;
+
+namespace LeetCode.Test.Y2024
 {
     public class UT13012024
     {
-
+        [Test]
+        public void LongestKSubstrTest()
+        {
+            var result = LC13012024.LongestKSubstr("aabacbebebe", 3);
+        }
     }
 }
--- LeetCode/Y2024/LC13012024.cs
+++ LeetCode/Y2024/LC13012024.cs
@@ -2,6 +2,41 @@
 {
     public class LC13012024
     {
+        /// <summary>
+        /// https://www.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1
+        /// i & j points to start index. j moves till we have size of dictionary less than equal to k moment dictionary size is more than k we move i and reduce frequency in dictionary. if frequency becomes 0 we remove key from dictionary
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int LongestKSubstr(string s, int k)
+        {
+            int i = 0, j = 0;
+            int result = 0;
+            Dictionary<char, int> kv = new Dictionary<char, int>();
+            while (j < s.Length - 1)
+            {
+                if (kv.ContainsKey(s[j]))
+                {
+                    kv[s[j]]++;
+                }
+                else
+                {
+                    if (kv.Count == k)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        result = Math.Max(result, j - i + 1);
+                        kv.Add(s[j], 1);
+                    }
+                }
+                j++;
 
+            }
+
+            return result;
+        }
     }
 }
