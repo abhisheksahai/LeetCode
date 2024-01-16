df4f2b74ba8f437e4d78ed7ec9acacd7434e6a8f
Abhishek Sahai <abhishek.sahai@gartner.com>
2024-01-16 11:07:26 +0530

    Remove extra space

--- LeetCode.Test/Y2024/UT14012024.cs
+++ LeetCode.Test/Y2024/UT14012024.cs
@@ -2,31 +2,32 @@
 
 namespace LeetCode.Test.Y2024
 {
-    public class UT14012024
-    {
-        [Test]
-        public void ConvertDecimalToBinaryTest()
-        {
-            var result = LC14012024.ConvertDecimalToBinary(10);
-        }
+	public class UT14012024
+	{
+		[Test]
+		public void ConvertDecimalToBinaryTest()
+		{
+			var result = LC14012024.ConvertDecimalToBinary(10);
+		}
 
-        [Test]
-        public void MissingNumberTest()
-        {
-            var result = LC14012024.MissingNumber([1, 2, 0]);
-        }
+		[Test]
+		public void MissingNumberTest()
+		{
+			var result = LC14012024.MissingNumber([1, 2, 0]);
+		}
 
-        [Test]
-        public void LeftShiftTest()
-        {
-            var result = LC14012024.LeftShift(2, 5);
-        }
+		[Test]
+		public void LeftShiftTest()
+		{
+			var result = LC14012024.LeftShift(2, 5);
+		}
 
-        [Test]
-        public void IsKBitSetForNumberTest()
-        {
-            var result1 = LC14012024.IsKBitSetForNumber(10, 3);
-            var result2 = LC14012024.IsKBitSetForNumber(10, 2);
-        }
-    }
+		[Test]
+		public void IsKBitSetForNumberTest()
+		{
+			var result1 = LC14012024.IsKBitSetForNumber(10, 3);
+			var result2 = LC14012024.IsKBitSetForNumber(10, 2);
+		}
+
+	}
 }
