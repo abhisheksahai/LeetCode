bf28551191013001df1c50e71925b1e584aba479
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-01-17 00:25:25 +0530

    ClearKBit

--- LeetCode.Test/Y2024/UT14012024.cs
+++ LeetCode.Test/Y2024/UT14012024.cs
@@ -2,38 +2,44 @@
 
 namespace LeetCode.Test.Y2024
 {
-	public class UT14012024
-	{
-		[Test]
-		public void ConvertDecimalToBinaryTest()
-		{
-			var result = LC14012024.ConvertDecimalToBinary(10);
-		}
+    public class UT14012024
+    {
+        [Test]
+        public void ConvertDecimalToBinaryTest()
+        {
+            var result = LC14012024.ConvertDecimalToBinary(10);
+        }
 
-		[Test]
-		public void MissingNumberTest()
-		{
-			var result = LC14012024.MissingNumber([1, 2, 0]);
-		}
+        [Test]
+        public void MissingNumberTest()
+        {
+            var result = LC14012024.MissingNumber([1, 2, 0]);
+        }
 
-		[Test]
-		public void LeftShiftTest()
-		{
-			var result = LC14012024.LeftShift(2, 5);
-		}
+        [Test]
+        public void LeftShiftTest()
+        {
+            var result = LC14012024.LeftShift(2, 5);
+        }
 
-		[Test]
-		public void IsKBitSetForNumberTest()
-		{
-			var result1 = LC14012024.IsKBitSetForNumber(10, 3);
-			var result2 = LC14012024.IsKBitSetForNumber(10, 2);
-		}
+        [Test]
+        public void IsKBitSetForNumberTest()
+        {
+            var result1 = LC14012024.IsKBitSetForNumber(10, 3);
+            var result2 = LC14012024.IsKBitSetForNumber(10, 2);
+        }
 
-		[Test]
-		public void SetKBitForNumberTest()
-		{
-			var result = LC14012024.SetKBitForNumber(10, 3);
-		}
+        [Test]
+        public void SetKBitForNumberTest()
+        {
+            var result = LC14012024.SetKBitForNumber(10, 3);
+        }
 
-	}
+        [Test]
+        public void ClearKBitTest()
+        {
+            var result = LC14012024.ClearKBit(10, 2);
+        }
+
+    }
 }
--- LeetCode/Y2024/LC14012024.cs
+++ LeetCode/Y2024/LC14012024.cs
@@ -1,64 +1,72 @@
 ﻿namespace LeetCode.Y2024
 {
-	public class LC14012024
-	{
-		public static int[] ConvertDecimalToBinary(int num)
-		{
-			Stack<int> stack = [];
-			while (num > 0)
-			{
-				int rem = num % 2;
-				stack.Push(rem);
-				num /= 2;
-			}
-			return stack.ToArray();
-		}
+    public class LC14012024
+    {
+        public static int[] ConvertDecimalToBinary(int num)
+        {
+            Stack<int> stack = [];
+            while (num > 0)
+            {
+                int rem = num % 2;
+                stack.Push(rem);
+                num /= 2;
+            }
+            return stack.ToArray();
+        }
 
-		/// <summary>
-		/// https://leetcode.com/problems/missing-number/
-		/// Tc=O(n) and SC=O(1)
-		/// </summary>
-		/// <param name="nums"></param>
-		/// <returns></returns>
-		public static int MissingNumber(int[] nums)
-		{
-			int xorProduct = 0;
-			for (int i = 0; i < nums.Length; i++)
-			{
-				xorProduct = xorProduct ^ nums[i];
-			}
-			for (int i = 1; i <= nums.Length; i++)
-			{
-				xorProduct = xorProduct ^ i;
-			}
-			return xorProduct;
-		}
+        /// <summary>
+        /// https://leetcode.com/problems/missing-number/
+        /// Tc=O(n) and SC=O(1)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int MissingNumber(int[] nums)
+        {
+            int xorProduct = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                xorProduct = xorProduct ^ nums[i];
+            }
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                xorProduct = xorProduct ^ i;
+            }
+            return xorProduct;
+        }
 
-		public static int LeftShift(int num, int k)
-		{
-			for (int i = 1; i <= k; i++)
-			{
-				num <<= 1;
-			}
-			return num;
-		}
+        public static int LeftShift(int num, int k)
+        {
+            for (int i = 1; i <= k; i++)
+            {
+                num <<= 1;
+            }
+            return num;
+        }
 
-		public static bool IsKBitSetForNumber(int num, int k)
-		{
-			int mask = 1 << (k - 1);
-			int result = num & mask;
-			if (result == 0)
-			{
-				return false;
-			}
-			return true;
-		}
+        public static bool IsKBitSetForNumber(int num, int k)
+        {
+            int mask = 1 << (k - 1);
+            int result = num & mask;
+            if (result == 0)
+            {
+                return false;
+            }
+            return true;
+        }
 
-		public static int SetKBitForNumber(int num, int k)
-		{
-			int mask = 1 << (k - 1);
-			num = num | mask;
-			return num;
-		}
-	}
+        public static int SetKBitForNumber(int num, int k)
+        {
+            int mask = 1 << (k - 1);
+            num = num | mask;
+            return num;
+        }
+
+        public static int ClearKBit(int num, int k)
+        {
+            int mask = 1 << (k - 1);
+            mask = (~mask);
+            num = num & mask;
+            return num;
+        }
+    }
 }
