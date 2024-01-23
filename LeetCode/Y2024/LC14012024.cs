5b2894ce2b6078e17e0a52e95e4c7fd49be771bb
Abhishek Sahai <abhishek.sahai@gartner.com>
2024-01-23 12:01:36 +0530

    Update LC14012024.cs

--- LeetCode/Y2024/LC14012024.cs
+++ LeetCode/Y2024/LC14012024.cs
@@ -1,73 +1,73 @@
 ﻿namespace LeetCode.Y2024
 {
-    public class LC14012024
-    {
-        public static int[] ConvertDecimalToBinary(int num)
-        {
-            Stack<int> stack = [];
-            while (num > 0)
-            {
-                int rem = num % 2;
-                stack.Push(rem);
-                num /= 2;
-            }
-            return stack.ToArray();
-        }
+	public class LC14012024
+	{
+		public static int[] ConvertDecimalToBinary(int num)
+		{
+			Stack<int> stack = [];
+			while (num > 0)
+			{
+				int rem = num % 2;
+				stack.Push(rem);
+				num /= 2;
+			}
+			return stack.ToArray();
+		}
 
-        /// <summary>
-        /// https://leetcode.com/problems/missing-number/
-        /// Tc=O(n) and SC=O(1)
-        /// </summary>
-        /// <param name="nums"></param>
-        /// <returns></returns>
-        public static int MissingNumber(int[] nums)
-        {
-            int xorProduct = 0;
-            for (int i = 0; i < nums.Length; i++)
-            {
-                xorProduct = xorProduct ^ nums[i];
-            }
-            for (int i = 1; i <= nums.Length; i++)
-            {
-                xorProduct = xorProduct ^ i;
-            }
-            return xorProduct;
-        }
+		/// <summary>
+		/// https://leetcode.com/problems/missing-number/
+		/// Tc=O(n) and SC=O(1)
+		/// </summary>
+		/// <param name="nums"></param>
+		/// <returns></returns>
+		public static int MissingNumber(int[] nums)
+		{
+			int xorProduct = 0;
+			for (int i = 0; i < nums.Length; i++)
+			{
+				xorProduct = xorProduct ^ nums[i];
+			}
+			for (int i = 1; i <= nums.Length; i++)
+			{
+				xorProduct = xorProduct ^ i;
+			}
+			return xorProduct;
+		}
 
-        public static int LeftShift(int num, int k)
-        {
-            for (int i = 1; i <= k; i++)
-            {
-                num <<= 1;
-            }
-            return num;
-        }
+		public static int LeftShift(int num, int k)
+		{
+			for (int i = 1; i <= k; i++)
+			{
+				num <<= 1;
+			}
+			return num;
+		}
 
-        public static bool IsKBitSetForNumber(int num, int k)
-        {
-            int mask = 1 << (k - 1);
-            int result = num & mask;
-            if (result == 0)
-            {
-                return false;
-            }
-            return true;
-        }
+		public static bool IsKBitSetForNumber(int num, int k)
+		{
+			int mask = 1 << (k - 1);
+			int result = num & mask;
+			if (result == 0)
+			{
+				return false;
+			}
+			return true;
+		}
 
-        public static int SetKBitForNumber(int num, int k)
-        {
-            int mask = 1 << (k - 1);
-            num = num | mask;
-            return num;
-        }
+		public static int SetKBitForNumber(int num, int k)
+		{
+			int mask = 1 << (k - 1);
+			num = num | mask;
+			return num;
+		}
 
-        public static int ClearKBit(int num, int k)
-        {
-            int mask = 1 << (k - 1);
-            mask = (~mask);
-            num = num & mask;
-            return num;
-        }
+		public static int ClearKBit(int num, int k)
+		{
+			int mask = 1 << (k - 1);
+			mask = (~mask);
+			num = num & mask;
+			return num;
+		}
 
 		/// <summary>
 		/// https://leetcode.com/problems/single-number-iii/
@@ -76,9 +76,16 @@
 		/// <returns></returns>
 		public static int[] SingleNumber(int[] nums)
 		{
-            int[] result = [];
+			int[] result = [];
 
-            return result;
+			return result;
+		}
+
+		public static double MyPow(double x, int n)
+		{
+			double result = 0;
+
+			return result;
 		}
 	}
 }
