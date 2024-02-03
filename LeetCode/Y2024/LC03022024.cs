db67dbf83e6667c9016e5d519212dceeb9f94baf
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-02-03 10:01:35 +0530

    AddTwoNumbers

--- LeetCode/Y2024/LC03022024.cs
+++ LeetCode/Y2024/LC03022024.cs
@@ -38,7 +38,49 @@
             }
             return prev;
         }
-    }
 
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/add-two-numbers/
+        /// MS,SF,Amazon
+        /// Tc=O(N) and SC=O(1)
+        /// </summary>
+        /// <param name="l1"></param>
+        /// <param name="l2"></param>
+        /// <returns></returns>
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode ans = new();
+            ListNode head = ans;
+            int sum = 0;
+            while (l1 != null || l2 != null || sum > 0)
+            {
+                if (l1 != null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+                ans.next = new ListNode(sum % 10);
+                sum = sum / 10;
+                ans = ans.next;
+            }
+            return head.next;
+        }
+    }
 
 }
