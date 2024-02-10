3eddeb40528ead1035cfc87e78f6be6194025bbb
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-02-10 09:47:49 +0530

    IsValid

--- LeetCode.Test/Y2024/UT10022024.cs
+++ LeetCode.Test/Y2024/UT10022024.cs
@@ -1,11 +1,20 @@
-﻿namespace LeetCode.Test.Y2024
+﻿using LeetCode.Y2024;
+
+namespace LeetCode.Test.Y2024
 {
     public class UT10022024
     {
         [Test]
-        public void Test()
+        public void ReverseStringTest()
         {
+            LC10022024.ReverseString(['h', 'e', 'l', 'l', 'o']);
+        }
 
+        [Test]
+        public void IsValidTest()
+        {
+            var result1 = LC10022024.IsValid("({[]})");
+            var result2 = LC10022024.IsValid("()");
         }
     }
 }
--- LeetCode/Y2024/LC10022024.cs
+++ LeetCode/Y2024/LC10022024.cs
@@ -1,7 +1,62 @@
-﻿namespace LeetCode.Y2024
+﻿using System.Collections;
+
+namespace LeetCode.Y2024
 {
+    //https://www.geeksforgeeks.org/c-sharp-stack-with-examples/
     public class LC10022024
     {
+        /// <summary>
+        /// https://leetcode.com/problems/reverse-string/
+        /// </summary>
+        /// <param name="s"></param>
+        public static void ReverseString(char[] s)
+        {
+            if (s == null || s.Length == 0)
+            {
+                return;
+            }
+            Stack<char> stack = new();
+            for (int i = 0; i < s.Length; i++)
+            {
+                stack.Push(s[i]);
+            }
+            int j = 0;
+            while (stack.Count > 0)
+            {
+                s[j++] = stack.Pop();
+            }
+        }
 
+        /// <summary>
+        /// https://leetcode.com/problems/valid-parentheses/
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool IsValid(string s)
+        {
+            Dictionary<char, char> dict = new()
+            {
+                { '{','}' },
+                { '(',')' },
+                { '[',']' }
+            };
+            Stack<char> stack = new();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.ContainsKey(s[i]))
+                {
+                    stack.Push(s[i]);
+                }
+                else if (stack.Count > 0 && dict[stack.Peek()] == s[i])
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return stack.Count == 0;
+        }
     }
 }
