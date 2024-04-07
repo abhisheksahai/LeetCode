f68d948b3fd0a336f45e452d4f85b8b402ad4017
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-04-07 09:08:45 +0530

    Heapify

--- LeetCode.Test/Y2024/April/UT01042024.cs
+++ LeetCode.Test/Y2024/April/UT01042024.cs
@@ -1,7 +1,13 @@
-﻿namespace LeetCode.Test.Y2024.April
+﻿using LeetCode.Y2024.April;
+
+namespace LeetCode.Test.Y2024.April
 {
     public class UT01042024
     {
-
+        [Test]
+        public void FindKthLargestTest()
+        {
+            var result = LC01042024.FindKthLargest([3, 2, 1, 5, 6, 4],2);
+        }
     }
 }
--- LeetCode/Y2024/April/LC01042024.cs
+++ LeetCode/Y2024/April/LC01042024.cs
@@ -25,5 +25,70 @@
             }
         }
 
+        /// <summary>
+        /// https://www.geeksforgeeks.org/problems/minimum-cost-of-ropes-1587115620/1
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static long MinCost(long[] arr, int n)
+        {
+            long cost = 0;
+            PriorityQueue<long, int> pq = new PriorityQueue<long, int>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                pq.Enqueue(arr[i], 0);
+            }
+            return cost;
+        }
+
+
+        /// <summary>
+        /// https://leetcode.com/problems/kth-largest-element-in-an-array/
+        /// SC=O(k) and TC=O(nlogk)
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int FindKthLargest(int[] nums, int k)
+        {
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                pq.Enqueue(nums[i], nums[i]);
+                if (pq.Count > k)
+                {
+                    pq.Dequeue();
+                }
+            }
+            return pq.Peek();
+        }
+
+        /// <summary>
+        /// This is handle internally by framework if we use priority queue.Its better to know the logic
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="N"></param>
+        /// <param name="i"></param>
+        static void Heapify(int[] arr, int N, int i)
+        {
+            int largest = i; // Initialize largest as root
+            int l = 2 * i + 1; // left = 2*i + 1
+            int r = 2 * i + 2; // right = 2*i + 2
+
+            if (l < N && arr[l] > arr[largest])
+                largest = l;
+
+            if (r < N && arr[r] > arr[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+                Heapify(arr, N, largest);
+            }
+        }
     }
 }
