a2a4e09f321de19054fbb002aa9443c27ec20aae
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-04-21 10:34:23 +0530

    Update LC21042024.cs

--- LeetCode/Y2024/April/LC21042024.cs
+++ LeetCode/Y2024/April/LC21042024.cs
@@ -32,5 +32,16 @@
         {
             return 0;
         }
+
+        /// <summary>
+        /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+        /// </summary>
+        /// <param name="prices"></param>
+        /// <param name="fee"></param>
+        /// <returns></returns>
+        public int MaxProfit(int[] prices, int fee)
+        {
+            return 0;
+        }
     }
 }
