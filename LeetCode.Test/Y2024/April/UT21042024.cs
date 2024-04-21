adb69a732b4b551a4340c54ccd41a103fc87c91e
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-04-21 09:24:37 +0530

    21-04-2024

---
+++ LeetCode.Test/Y2024/April/UT21042024.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Y2024.April;
+
+namespace LeetCode.Test.Y2024.April
+{
+    public class UT21042024
+    {
+        [Test]
+        public void NumDecodingsTest()
+        {
+            var result = LC21042024.NumDecodings("");
+        }
+    }
+}
---
+++ LeetCode/Y2024/April/LC21042024.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Y2024.April
+{
+    public class LC21042024
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/decode-ways/
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int NumDecodings(string s)
+        {
+            return 0;
+        }
+    }
+}
