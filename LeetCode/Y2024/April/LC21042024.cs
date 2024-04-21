4dab3c51509b99a0ea677db88aa619c07356d031
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-04-21 10:02:51 +0530

    Update LC21042024.cs

--- LeetCode/Y2024/April/LC21042024.cs
+++ LeetCode/Y2024/April/LC21042024.cs
@@ -11,5 +11,16 @@
         {
             return 0;
         }
+
+        /// <summary>
+        /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+        /// </summary>
+        /// <param name="k"></param>
+        /// <param name="prices"></param>
+        /// <returns></returns>
+        public int MaxProfit(int k, int[] prices)
+        {
+            return 0;
+        }
     }
 }
