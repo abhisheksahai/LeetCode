89b0089d5f858bdbcc9bf832d13a497e1c051087
Abhishek Sahai <abhishek.sahai@outlook.in>
2024-06-03 11:22:26 +0530

    twst

---
+++ LeetCode.Test/Y2024/May/UT11052024.cs
@@ -0,0 +1,12 @@
+﻿using LeetCode.Y2024.April;
+
+namespace LeetCode.Test.Y2024.May
+{
+    public class UT11052024
+    {
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
---
+++ LeetCode/Y2024/May/LC11052024.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Y2024.May
+{
+    public class LC11052024
+    {
+        /// <summary>
+        /// https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
+        /// </summary>
+        /// <param name="V"></param>
+        /// <param name="adj"></param>
+        /// <returns></returns>
+        public List<int> BfsOfGraph(int V, List<int>[] adj)
+        {
+            List<int> list = new List<int>();
+            return list;
+        }
+
+        public int[] DfsOfGraph(int V, List<int>[] adj)
+        {
+            int[] result = new int[V];
+            return result;
+        }
+    }
+}
